ad8b1946fbf9c76eca7a3480bbb61d9f3121e224
AlephZr <38972838+AlephZr@users.noreply.github.com>
2021-01-10 15:39:40 +0800

    Update cifar10_test.sh

--- scripts/cifar10_test.sh
+++ scripts/cifar10_test.sh
@@ -3,5 +3,5 @@ python test.py --dataset_name CIFAR10 \
        --gpu_id 1 \
        --netG EGAN32 --ngf 128 \
        --z_dim 100 \
-       --test_name IS --test_size 50000 --fid_batch_size 500 \
+       --test_name IS --test_size 50000 --fid_batch_size 100 \
        --savetag nsgan_lr=1e-4 \
